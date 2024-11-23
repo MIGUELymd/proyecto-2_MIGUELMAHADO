eab385790c471d3b6a7d5ceeb1e9c0c618e6ecec
Miguel <yonaikel.diaz27@gmail.com>
2024-11-23 12:01:46 -0400

    primera funcion funcional

--- LiBrErIa.js
+++ LiBrErIa.js
@@ -1,18 +1,28 @@
 //hola mi gente buena
 // segundo comentario x
-librosColeccion[
-{
-"nombre": "el principito",
+ let librosColeccion=[{
+"nombre":"el principito",
 "cantidadPaginas":100,
 "autor": "mario",
-"genero": ["drama ","romance"],
-"disponible" : true
-} ,
+"genero": ["drama","romance"],
+"disponible": true
+},
 {
-"nombre": "señor de los anillos",
+"nombre" : "señor de los anillos",
 "cantidaddepaginas":1304,
 "autor": "pedro",
 "genero": ["accion", "drama"],
 "disponible":true
 }
 ];
+
+function informacionLibro(libro){
+console.log(libro.nombre + " " + libro.cantidaddepaginas + " " +
++ libro.autor + " " + libro.genero + " " + libro.disponible) 
+};
+console.log(librosColeccion)
+
+function imprimirLibros(libros){
+
+    
+};
