f0ffd61f599eb581bb09f0742200d9d758216d88
Miguel <yonaikel.diaz27@gmail.com>
2024-11-23 10:57:22 -0400

    creacion de JSON

--- LiBrErIa.js
+++ LiBrErIa.js
@@ -1,2 +1,18 @@
 //hola mi gente buena
 // segundo comentario x
+librosColeccion[
+{
+"nombre": "el principito",
+"cantidadPaginas":100,
+"autor": "mario",
+"genero": ["drama ","romance"],
+"disponible" : true
+} ,
+{
+"nombre": "señor de los anillos",
+"cantidaddepaginas":1304,
+"autor": "pedro",
+"genero": ["accion", "drama"],
+"disponible":true
+}
+];
